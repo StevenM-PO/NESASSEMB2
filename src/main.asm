755c44c6a2a11b97f7217e3c056469a816b437a7
Steven <smitchellpo@gmail.com>
2020-03-20 14:52:39 -0700

    First Commit

---
+++ src/main.asm
@@ -0,0 +1,67 @@
+.include "constants.inc"
+.include "header.inc"
+
+.segment "ZEROPAGE"
+
+.segment "CODE"
+.proc irq_handler
+  RTI
+.endproc
+
+.proc nmi_handler
+  LDA #$00
+  STA OAMADDR
+  LDA #$02
+  STA OAMDMA
+  RTI
+.endproc
+
+.import reset_handler
+
+.export main
+.proc main
+  LDX PPUSTATUS
+  LDX #$3F
+  STX PPUADDR
+  LDX #$00
+  STX PPUADDR
+  LDX #$00
+load_palettes:
+  LDA palettes,X
+  STA PPUDATA
+  INX
+  CPX #$10
+  BNE load_palettes
+  LDX #$00
+load_spritePalettes:
+  LDA palettes,X
+  STA PPUDATA
+  INX
+  CPX #$10
+  BNE load_spritePalettes
+  LDX #$00
+
+vblankwait:
+  BIT PPUSTATUS
+  BPL vblankwait
+  LDA #%10010000
+  STA PPUCTRL
+  LDA #%00111110
+  STA PPUMASK
+
+  JMP vblankwait
+.endproc
+
+.segment "VECTORS"
+.addr nmi_handler, reset_handler, irq_handler
+
+.segment "CHR"
+.incbin "graphics.chr"
+
+.segment "RODATA"
+palettes:
+palettes:
+.byte $11, $19, $09, $0f
+.byte $23, $01, $05, $35
+.byte $23, $01, $05, $35
+.byte $23, $01, $05, $35
